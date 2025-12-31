421baba36c328f3f47588dcb624008e03999e4b6
Pratap Padhy <pratappadhy2005@gmail.com>
2025-12-31 18:19:48 +1100

    -Added remote backend example

---
+++ AWS/day_04/main.tf
@@ -0,0 +1,35 @@
+terraform {
+
+  backend "s3" {
+    bucket       = "pratap-bucket-tf-state"
+    key          = "dev/terraform.tfstate"
+    region       = "ap-southeast-2"
+    encrypt      = true
+    use_lockfile = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+#
+
+# create s3 bucket
+resource "aws_s3_bucket" "first_bucket" {
+  bucket = "pratap-bucket-001"
+
+  tags = {
+    Name          = "Example Bucket"
+    Environment   = "Dev"
+    Documentation = "https://www.terraform.io/docs/providers/aws/r/s3_bucket.html"
+  }
+}
