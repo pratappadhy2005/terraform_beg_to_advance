4d452d761cc42552d33228871a1fa8f438c5ef46
Pratap Padhy <pratappadhy2005@gmail.com>
2026-01-02 16:58:17 +1100

    -Added input variable

--- AWS/day_04/main.tf
+++ AWS/day_04/main.tf
@@ -21,15 +21,39 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
-#
+#create a variable
+variable "environment" {
+  default     = "Dev"
+  description = "The environment to deploy to"
+  type        = string
+}
 
 # create s3 bucket
 resource "aws_s3_bucket" "first_bucket" {
   bucket = "pratap-bucket-001"
 
   tags = {
-    Name          = "Example Bucket"
-    Environment   = "Dev"
+    Name          = "${var.environment}-Bucket"
+    Environment   = var.environment
     Documentation = "https://www.terraform.io/docs/providers/aws/r/s3_bucket.html"
+  }
+}
+
+#create a vpc
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name        = "${var.environment}-VPC"
+    Environment = var.environment
+  }
+}
+
+#create a EC2
+resource "aws_ec2_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  tags = {
+    Name        = "${var.environment}-EC2"
+    Environment = var.environment
   }
 }
